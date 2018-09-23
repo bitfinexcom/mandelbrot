b198d58be007d562984034028186f8ce12bf0900
Robert Kowalski <rok@kowalski.gd>
2018-09-23 12:22:31 +0100

    add basic test

---
+++ test/mandelbrot-ws-hive.js
@@ -0,0 +1,55 @@
+/* eslint-env mocha */
+
+'use strict'
+const assert = require('assert')
+
+const MWsHive = require('../').WsBase
+const Wock = require('./ws-testhelper.js')
+
+describe('websockets', () => {
+  it('connects and disconnects, sends messages', (done) => {
+    // tests basic functions:
+    // 1. connect to server
+    // 2. send message
+    // 3. receive response
+    // 4. close connection
+
+    const wss = new Wock({
+      port: 8888
+    })
+
+    wss.messageHook = (ws, msg) => {
+      assert.equal(msg.event, 'subscribe')
+      wss.send(ws, {ok: true})
+    }
+
+    wss.closeHook = (ws) => {
+      wss.close()
+    }
+
+    const conf = { url: 'ws://localhost:8888' }
+    const ws = new MWsHive(conf)
+
+    ws.on('open', () => {
+      const msg = {
+        event: 'subscribe',
+        channel: 'book',
+        symbol: 'BTCUSD'
+      }
+
+      ws.send(msg)
+    })
+
+    ws.on('close', () => {
+      assert.equal(ws.connected, false)
+      done()
+    })
+
+    ws.on('message', (msg) => {
+      assert.deepEqual(msg, { ok: true })
+      ws.close()
+    })
+
+    ws.open()
+  })
+})
