c156c933d55ff6306aa0d4456f07ccf9f8a97761
Robert Kowalski <rok@kowalski.gd>
2018-09-23 12:17:24 +0100

    standalone module fixes

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 exports.WsBase = require('./lib/mandelbrot-ws-base.js')
 
-exports.BaseWallet = require('./lib/managed-wallet.js')
-exports.BaseOrderbook = require('./lib/managed-orders.js')
-exports.BaseOrders = require('./lib/managed-orders.js')
+exports.BaseWallet = require('./lib/base-wallet.js')
+exports.BaseOrderbook = require('./lib/base-ob.js')
+exports.BaseOrders = require('./lib/base-orders.js')
