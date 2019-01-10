c50c29dfb0668f8e5ccd7d6f74a3bea6a713e881
Robert Kowalski <rok@kowalski.gd>
2019-01-10 14:38:31 +0100

    add p0 orderbooks

--- index.js
+++ index.js
@@ -7,3 +7,4 @@ exports.BaseOrderbook = require('./lib/base-ob.js')
 exports.BaseOrders = require('./lib/base-orders.js')
 
 exports.R0Orderbook = require('./lib/orderbook-r0.js')
+exports.P0Orderbook = require('./lib/orderbook-p0.js')
