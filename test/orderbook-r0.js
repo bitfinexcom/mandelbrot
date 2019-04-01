4931af3b79ef869f46e47c247126258fd05aa383
Robert Kowalski <rok@kowalski.gd>
2019-04-01 13:38:39 +0200

    r0 orderbook: fix partial updates
    fixes an issue where partial fills were not updated properly

--- test/orderbook-r0.js
+++ test/orderbook-r0.js
@@ -254,4 +254,148 @@ describe('orderbook helper', () => {
     assert.deepStrictEqual(update, o.parse(update))
     o.parse(update)
   })
+
+  it('supports partial matches and price updates, first item', () => {
+    const o = new Orderbook()
+    const snap = [
+      ['18446744073709539151', 1.020000, 13.352000],
+      ['18446744073709539180', 1.010000, 1.000000],
+      ['18446744073709539685', 1.000000, 89.000000]
+    ]
+
+    o.update(snap)
+    assert.deepStrictEqual(snap, o.getState())
+
+    const update = ['18446744073709539151', 1.030000, 13.252000]
+    o.update(update)
+
+    const exp = [
+      ['18446744073709539151', 1.030000, 13.252000],
+      ['18446744073709539180', 1.010000, 1.000000],
+      ['18446744073709539685', 1.000000, 89.000000]
+    ]
+
+    assert.deepStrictEqual(exp, o.getState())
+  })
+
+  it('supports partial matches and price updates, middle item', () => {
+    const o = new Orderbook()
+    const snap = [
+      ['18446744073709539151', 1.020000, 13.352000],
+      ['18446744073709539180', 1.010000, 1.000000],
+      ['18446744073709539685', 1.000000, 89.000000],
+      ['12415', 3.999800, -30.000000],
+      ['12227', 8.999900, -0.836200],
+      ['9545', 50.000000, -0.010000]
+    ]
+
+    o.update(snap)
+    assert.deepStrictEqual(snap, o.getState())
+
+    const update = ['18446744073709539180', 1.020000, 1.100000]
+    o.update(update)
+
+    const exp = [
+      ['18446744073709539151', 1.020000, 13.352000],
+      ['18446744073709539180', 1.020000, 1.100000],
+      ['18446744073709539685', 1.000000, 89.000000],
+      ['12415', 3.999800, -30.000000],
+      ['12227', 8.999900, -0.836200],
+      ['9545', 50.000000, -0.010000]
+    ]
+
+    assert.deepStrictEqual(exp, o.getState())
+  })
+
+  it('supports partial matches and price updates, first item, keyed snap', () => {
+    const o = new Orderbook({ keyed: true })
+    const snap = [
+      ['18446744073709539151', 1.020000, 13.352000],
+      ['18446744073709539180', 1.010000, 1.000000],
+      ['18446744073709539685', 1.000000, 89.000000],
+      ['12415', 3.999800, -30.000000],
+      ['12227', 8.999900, -0.836200],
+      ['9545', 50.000000, -0.010000]
+    ]
+
+    o.update(snap)
+
+    assert.deepStrictEqual({
+      asks: [
+        { id: '9545', price: 50, amount: -0.01 },
+        { id: '12227', price: 8.9999, amount: -0.8362 },
+        { id: '12415', price: 3.9998, amount: -30 }
+      ],
+      bids: [
+        { id: '18446744073709539685', price: 1, amount: 89 },
+        { id: '18446744073709539180', price: 1.01, amount: 1 },
+        { id: '18446744073709539151', price: 1.02, amount: 13.352 }
+      ]
+    }, o.getState())
+
+    const updateBids = ['18446744073709539151', 1.030000, 13.252000]
+    o.update(updateBids)
+
+    const updateAsks = ['9545', 51, -0.02]
+    o.update(updateAsks)
+
+    assert.deepStrictEqual({
+      asks: [
+        { id: '9545', price: 51, amount: -0.02 },
+        { id: '12227', price: 8.9999, amount: -0.8362 },
+        { id: '12415', price: 3.9998, amount: -30 }
+      ],
+      bids: [
+        { id: '18446744073709539685', price: 1, amount: 89 },
+        { id: '18446744073709539180', price: 1.01, amount: 1 },
+        { id: '18446744073709539151', price: 1.03, amount: 13.252 }
+      ]
+    }, o.getState())
+  })
+
+  it('supports partial matches and price updates, middle item, keyed snap', () => {
+    const o = new Orderbook({ keyed: true })
+    const snap = [
+      ['18446744073709539151', 1.020000, 13.352000],
+      ['18446744073709539180', 1.010000, 1.000000],
+      ['18446744073709539685', 1.000000, 89.000000],
+      ['12415', 3.999800, -30.000000],
+      ['12227', 8.999900, -0.836200],
+      ['9545', 50.000000, -0.010000]
+    ]
+
+    o.update(snap)
+
+    assert.deepStrictEqual({
+      asks: [
+        { id: '9545', price: 50, amount: -0.01 },
+        { id: '12227', price: 8.9999, amount: -0.8362 },
+        { id: '12415', price: 3.9998, amount: -30 }
+      ],
+      bids: [
+        { id: '18446744073709539685', price: 1, amount: 89 },
+        { id: '18446744073709539180', price: 1.01, amount: 1 },
+        { id: '18446744073709539151', price: 1.02, amount: 13.352 }
+      ]
+    }, o.getState())
+
+    const updateBids = ['18446744073709539180', 1.04, 1.111]
+    o.update(updateBids)
+
+    const updateAsks = ['12227', 8.9998, -0.8361]
+    o.update(updateAsks)
+
+    assert.deepStrictEqual({
+      asks: [
+        { id: '9545', price: 50, amount: -0.01 },
+        { id: '12227', price: 8.9998, amount: -0.8361 },
+        { id: '12415', price: 3.9998, amount: -30 }
+      ],
+      bids: [
+        { id: '18446744073709539685', price: 1, amount: 89 },
+        { id: '18446744073709539180', price: 1.04, amount: 1.111 },
+        { id: '18446744073709539151', price: 1.02, amount: 13.352 }
+      ]
+    }, o.getState())
+  })
 })
